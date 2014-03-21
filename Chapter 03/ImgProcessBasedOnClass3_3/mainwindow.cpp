b69dba8a2aeb611e35f17498a5e1378c1fd9c5a9
bigfool <qizhenfang@qq.com>
2014-03-21 12:39:09 +0800

    在MainWindow类，增加两个按钮，调用ColorDetectController类的对象，完成改写

--- Chapter 03/ImgProcessBasedOnClass3_3/mainwindow.cpp
+++ Chapter 03/ImgProcessBasedOnClass3_3/mainwindow.cpp
@@ -6,9 +6,36 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->processButton->setEnabled(false);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+    if (m_colorDetectorCtl != NULL)
+        delete m_colorDetectorCtl;
+}
+
+void MainWindow::on_openButton_clicked()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open Image"), ".",
+                        tr("Image Files(*.png *.jpg *.jpeg *.bmp)"));
+
+    StrategyColorDetector *stg = new StrategyColorDetector();
+    m_colorDetectorCtl = new ColorDetectController((ProcessStrategy *)stg);
+    if (m_colorDetectorCtl->setInputImage(fileName.toUtf8().data()))
+    {
+        ui->processButton->setEnabled(true);
+    }
+
+    cv::namedWindow("original");
+    cv::imshow("original", m_colorDetectorCtl->getInputImage());
+}
+
+void MainWindow::on_processButton_clicked()
+{
+    m_colorDetectorCtl->setTargetColor(130, 190, 200);
+    m_colorDetectorCtl->doProcess();
+    cv::namedWindow("result");
+    cv::imshow("result", m_colorDetectorCtl->getLastResult());
 }
