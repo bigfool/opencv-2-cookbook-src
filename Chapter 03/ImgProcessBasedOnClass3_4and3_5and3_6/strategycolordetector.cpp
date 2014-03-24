eb92fb0a7e196e353bcf48e19a0484d773f7ce08
bigfool <qizhenfang@qq.com>
2014-03-24 21:56:11 +0800

    将颜色空间由BGR转换为CIE L*a*b空间，同时增加了几个调试输出

--- Chapter 03/ImgProcessBasedOnClass3_4and3_5and3_6/strategycolordetector.cpp
+++ Chapter 03/ImgProcessBasedOnClass3_4and3_5and3_6/strategycolordetector.cpp
@@ -1,3 +1,4 @@
+#include <opencv2/imgproc/imgproc.hpp>
 #include "strategycolordetector.h"
 
 StrategyColorDetector::StrategyColorDetector()
@@ -30,14 +31,26 @@ int StrategyColorDetector::getColorDistanceThreshold() const
 
 void StrategyColorDetector::setTargetColor(unsigned char red, unsigned char green, unsigned char blue)
 {
-    m_target[2] = red;
-    m_target[1] = green;
-    m_target[0] = blue;
+//    m_target[2] = red;
+//    m_target[1] = green;
+//    m_target[0] = blue;
+    cv::Mat tmp(1, 1, CV_8UC3);
+    tmp.at<cv::Vec3b>(0, 0)[0] = blue;
+    tmp.at<cv::Vec3b>(0, 0)[1] = green;
+    tmp.at<cv::Vec3b>(0, 0)[2] = red;
+
+    cv::cvtColor(tmp, tmp, CV_BGR2Lab);
+    m_target = tmp.at<cv::Vec3b>(0, 0);
 }
 
 void StrategyColorDetector::setTargetColor(cv::Vec3b color)
 {
-    m_target = color;
+//    m_target = color;
+    cv::Mat tmp(1, 1, CV_8UC3);
+    tmp.at<cv::Vec3b>(0, 0) = color;
+    cv::cvtColor(tmp, tmp, CV_BGR2Lab);
+
+    m_target = tmp.at<cv::Vec3b>(0, 0);
 }
 
 cv::Vec3b StrategyColorDetector::getTargetColor() const
@@ -55,30 +68,42 @@ int StrategyColorDetector::getDistance(const cv::Vec3b& color) const
 cv::Mat StrategyColorDetector::ProcessAlgrithmIf(const cv::Mat &image)
 {
     qDebug()  << "[StrategyColorDetector::ProcessAlgrithmIf] ProcessAlgrithmIf ";
+//    cv::namedWindow("debug");
+//    cv::imshow("debug", image);
     m_result.create(image.rows, image.cols, CV_8U);
 
-    cv::Mat_<cv::Vec3b>::const_iterator it = image.begin<cv::Vec3b>();
-    cv::Mat_<cv::Vec3b>::const_iterator itend = image.end<cv::Vec3b>();
-    cv::Mat_<cv::Vec3b>::iterator itout = m_result.begin<cv::Vec3b>();
+    cv::Mat converted;
+    converted.create(image.rows, image.cols, image.type());
+
+    cv::cvtColor(image, converted, CV_BGR2Lab); //in opencv2/imgproc/imgproc.hpp
+    cv::namedWindow("debug");
+    cv::imshow("debug", converted);
+    cv::Mat_<cv::Vec3b>::iterator it = converted.begin<cv::Vec3b>();
+    cv::Mat_<cv::Vec3b>::iterator itend = converted.end<cv::Vec3b>();
+    cv::Mat_<uchar>::iterator itout = m_result.begin<uchar>();
 
     for (; it != itend; it++, itout++)
     {
         if(getDistance(*it) < m_minDist)
         {
+            qDebug()  << "[StrategyColorDetector::ProcessAlgrithmIf] getDistance " << getDistance(*it);
             *itout = 255;
         }
         else
         {
+            qDebug()  << "[StrategyColorDetector::ProcessAlgrithmIf] getDistance " << getDistance(*it);
             *itout = 0;
         }
     }
 
+    cv::namedWindow("debug result");
+    cv::imshow("debug result", m_result);
     return m_result;
 }
 
 void StrategyColorDetector::setTargetColorIf(unsigned char red, unsigned char green, unsigned char blue)
 {
-     qDebug() << QString("[ProcessStrategy::setTargetColorIf]red = %1 green = %2 blue = %3").arg(red).arg(green).arg(blue);
+     qDebug() << QString("[StrategyColorDetector::setTargetColorIf]red = %1 green = %2 blue = %3").arg(red).arg(green).arg(blue);
     setTargetColor(red, green, blue);
 }
 
